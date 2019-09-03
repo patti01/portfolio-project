2b140a220f7a02b993a82d2953df0e915227b2b1
Patricia Falandysz <pfalandysz@yahoo.com>
2019-09-03 19:14:35 +0200

    section added with svg and wave

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,27 +1,52 @@
 import React from 'react'
 import styled from 'styled-components'
+import './Section.css'
+import Wave from './Wave';
 
 const SectionGroup = styled.div`
-    background: black; 
-    height: 720px;
+    background: white; 
+    position: relative; 
+    z-index: 5;
+    overflow: hidden; 
 `
 
 const SectionTitleGroup = styled.div`
+    z-index: 5;
 `
 
 const SectionTitle = styled.h3`
 `
 
-const SectionText = styled.p`
+const SectionText = styled.div`
+    background: #00008B;
+    z-index: 5;
 `
 
-const Section = props => (
-    <SectionGroup image ={props.image}>
-        <SectionTitleGroup>
-            <SectionTitle>{props.title}</SectionTitle>
-            <SectionText>{props.text}</SectionText>
-        </SectionTitleGroup>
-    </SectionGroup>
+const WaveBottom = styled.div`
+    transform: rotate(180deg);
+`
+
+const Section = props => ( 
+        <SectionGroup image ={props.image}>
+            <img src={require('../images/quadri.jpeg')} id="quadri" alt="quadri"/>
+            <svg id="circleRed" xmlns="http://www.w3.org/2000/svg" width="500" height="500">
+                <circle fill="#E50000" cx="350.674" cy="351.169" r="346.067"/>
+            </svg>
+            <Wave />
+            <SectionTitleGroup>
+                {/*<SectionTitle>{props.title}</SectionTitle>
+                <SectionText>{props.text}</SectionText>*/}
+                <SectionText className="SectionSkills">
+                <p data-text="Brand Identity">Brand Identity</p>
+                <p data-text="Front-End">Front-End</p>
+                <p data-text="Art Direction">Art Direction</p>
+                <p data-text="Strategy">Strategy</p>
+                <p data-text="Webdesign">Webdesign</p>
+                <p data-text="User Experience">User Experience</p>
+                </SectionText>
+            </SectionTitleGroup>
+            <WaveBottom><Wave /></WaveBottom>
+        </SectionGroup>
 )
 
 export default Section
--- src/components/header.js
+++ src/components/header.js
@@ -33,8 +33,8 @@ class Header extends React.Component {
             <Link to="/"><img src={require('../images/logo-bauhaus-black.png')} width="30"/></Link>
           </div>
           <div className="HeaderNav">
-            <Link to="/works" className="menu-item">Works</Link>
-            <Link to="/playground" className="menu-item">Playground</Link>
+            <Link to="/works" className="menu-item"><div className="bouncingLetter"><span>W</span><span>o</span><span>r</span><span>k</span><span>s</span></div></Link>
+            <Link to="/playground" className="menu-item"><div className="bouncingLetter"><span>P</span><span>l</span><span>a</span><span>y</span><span>g</span><span>r</span><span>o</span><span>u</span><span>n</span><span>d</span></div></Link>
             <Link to="/contact"><button>Contact</button></Link>
           </div>
         </div>
